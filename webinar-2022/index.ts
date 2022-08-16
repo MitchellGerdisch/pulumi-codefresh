e2fa45a9b729089bcf67c91902f44869677bbd8d
MitchellGerdisch <mitch@pulumi.com>
2022-08-16 14:32:58 -0500

    alb ingress controller and dns validation

--- webinar-2022/index.ts
+++ webinar-2022/index.ts
@@ -1,9 +1,14 @@
 import * as pulumi from "@pulumi/pulumi"
 import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
+import * as k8s from "@pulumi/kubernetes";
 import { local } from "@pulumi/command";
 import { StackTag } from "@pulumi/pulumiservice"
 import * as fs from "fs";
+
+import { AlbIngressController } from "./alb-ing-cntlr";
+import * as dns from "./dns";
+export const domainName = dns.domainName;
 
 const config = new pulumi.Config();
 const codefreshApiKey = config.getSecret("codefreshApiKey");
@@ -24,6 +29,28 @@ const cluster = new eks.Cluster(name, {
     desiredCapacity: 2,
     minSize: 1,
     maxSize: 2,
+    createOidcProvider: true,
+});
+
+// get the cluster OIDC provider URL.
+if (!cluster?.core?.oidcProvider) {
+    throw new Error("Invalid cluster OIDC provider URL");
+}
+const clusterOidcProvider = cluster.core.oidcProvider;
+const clusterOidcProviderArn = clusterOidcProvider.arn;
+const clusterOidcProviderUrl = clusterOidcProvider.url;
+
+// Instantiate K8s provider using the kubeconfig from the cluster create above. 
+const provider = new k8s.Provider("k8s-provider", {kubeconfig: cluster.kubeconfig});
+
+// Deploy ALB Ingress Controller.
+const albIngCntlr = new AlbIngressController(name, {
+    namespace: "kube-system",
+    provider: cluster.provider,
+    vpcId: vpc.id,
+    clusterName: cluster.eksCluster.name,
+    clusterOidcProviderArn: clusterOidcProviderArn,
+    clusterOidcProviderUrl: clusterOidcProviderUrl,
 });
 
 // If an API key for Codefresh is configured, then register the cluster with Codefresh.
