af48da96ca945bc4c4076b139a1da0be8dc95e60
MitchellGerdisch <mitch@pulumi.com>
2022-08-23 16:01:36 -0500

    clean up

--- webinar-2022/cluster-services/index.ts
+++ webinar-2022/cluster-services/index.ts
@@ -4,34 +4,9 @@ import * as pulumi from "@pulumi/pulumi";
 import { local } from "@pulumi/command";
 import { StackTag } from "@pulumi/pulumiservice"
 import { config } from "./config";
-import { RdsDatabase } from "./rds-db";
 import { FluentdCloudWatch } from "./fluentd-cloudwatch";
 import { ExternalDns } from "./external-dns";
 import { AlbIngressController } from "./alb-ing-cntlr";
-
-
-//// // Deploy RDS Aurora DB
-// const rds = new RdsDatabase("rds-aurora-db", {
-//     privateSubnetIds: config.privateSubnetIds,
-//     securityGroupId : config.nodeSecurityGroupId,
-//     replicas: config.dbReplicas,
-//     instanceType: config.dbInstanceType,
-// });
-// const db = rds.db;
-
-// // Export the DB connection information.
-// interface DbConn {
-//     host: pulumi.Output<string>;
-//     port: pulumi.Output<string>;
-//     username: pulumi.Output<string>;
-//     password: pulumi.Output<string>;
-// }
-// export const dbConn: DbConn = {
-//     host: db.endpoint,
-//     port: db.port.apply(port => port.toString()),
-//     username: db.masterUsername,
-//     password: rds.password, // db.masterPassword can possibly be undefined. Use rds.password instead.
-//// };
 
 const project = pulumi.getProject();
 const stack = pulumi.getStack();
